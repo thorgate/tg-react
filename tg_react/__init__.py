69a0442b41dfefe8d673d4f4d8530de66814588c
Nick Pope <nick.pope@infogrid.io>
2022-07-01 22:50:45 +0100

    Fix `default_app_config` deprecation warning

--- tg_react/__init__.py
+++ tg_react/__init__.py
@@ -2,4 +2,10 @@ VERSION = (3, 0, 0)
 
 __version__ = "3.0.0"
 
-default_app_config = "tg_react.apps.TgReactConfig"
+try:
+    import django
+except ModuleNotFoundError:
+    pass
+else:
+    if django.VERSION < (3, 2):
+        default_app_config = "tg_react.apps.TgReactConfig"
