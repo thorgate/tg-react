8bf0f947a53ada7ec8ef64d2bb7c7562bcab4587
Nick Pope <nick.pope@infogrid.io>
2022-07-01 23:11:13 +0100

    Remove obsolete unicode string prefix

--- docs/conf.py
+++ docs/conf.py
@@ -44,8 +44,8 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = u'tg-react'
-copyright = u'2015-present, Thorgate'
+project = 'tg-react'
+copyright = '2015-present, Thorgate'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
@@ -190,8 +190,8 @@ latex_elements = {
 # Grouping the document tree into LaTeX files. List of tuples
 # (source start file, target name, title, author, documentclass [howto/manual]).
 latex_documents = [
-  ('index', 'tg-react.tex', u'tg-react Documentation',
-   u'Thorgate', 'manual'),
+  ('index', 'tg-react.tex', 'tg-react Documentation',
+   'Thorgate', 'manual'),
 ]
 
 # The name of an image file (relative to this directory) to place at the top of
@@ -220,8 +220,8 @@ latex_documents = [
 # One entry per manual page. List of tuples
 # (source start file, name, description, authors, manual section).
 man_pages = [
-    ('index', 'tg-react', u'tg-react Documentation',
-     [u'Thorgate'], 1)
+    ('index', 'tg-react', 'tg-react Documentation',
+     ['Thorgate'], 1)
 ]
 
 # If true, show URL addresses after external links.
@@ -234,8 +234,8 @@ man_pages = [
 # (source start file, target name, title, author,
 #  dir menu entry, description, category)
 texinfo_documents = [
-  ('index', 'tg-react', u'tg-react Documentation',
-   u'Thorgate', 'tg-react', 'One line description of project.',
+  ('index', 'tg-react', 'tg-react Documentation',
+   'Thorgate', 'tg-react', 'One line description of project.',
    'Miscellaneous'),
 ]
 
